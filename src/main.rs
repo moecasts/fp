391a1d69006de0ba0563573988f6ce741070e067
Caster <moecasts.caster@gmail.com>
2022-10-09 10:01:48 +0800

    feat: 支持多字音形编码查询

--- src/main.rs
+++ src/main.rs
@@ -17,19 +17,22 @@ fn main() {
 
     let f = File::open(dict_path).unwrap();
     let dict: serde_json::Value = serde_json::from_reader(f).unwrap();
+    
+    for ch in input.chars() {
+        match dict[String::from(ch)].as_array() {
+            Some(item) => {
+                println!("{}", &item[0].as_str().unwrap());
 
-    match dict[input].as_array() {
-        Some(item) => {
-            println!("{}", &item[0].as_str().unwrap());
+                println!("===============================================");
 
-            println!("===============================================");
-
-            println!("拆　分：  {}", item[1].as_str().unwrap());
-            println!("首　末：  {}  {}", item[2].as_str().unwrap(), item[3].as_str().unwrap());
-            println!("编  码：  {}  {}", item[4].as_str().unwrap(), item[5].as_str().unwrap());
-        },
-        None => {
-            println!("未收录");
+                println!("拆　分：  {}", item[1].as_str().unwrap());
+                println!("首　末：  {}  {}", item[2].as_str().unwrap(), item[3].as_str().unwrap());
+                println!("编  码：  {}  {}", item[4].as_str().unwrap(), item[5].as_str().unwrap());
+            },
+            None => {
+                println!("未收录");
+            }
         }
+        println!("");
     }
 }
